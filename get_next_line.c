4edbf76dd9af8107a4c48820b729c3d0f6437717
Nataliia KOLOSOVA <nkolosov@e1r8p10.unit.ua>
2017-11-20 16:44:13 +0200

    no norm. Multiple descriptor support.

--- get_next_line.c
+++ get_next_line.c
@@ -18,60 +18,66 @@
 
 #include <stdio.h>
 
-static char	*list_to_string(t_list *t)
+t_list	*get_list_elem(t_list **l, const int fd)
 {
-	char	*ans;
-	char	*str;
-	int		i;
-	int		j;
-	t_list	*l;
+	t_list	*t;
 
-	l = t;
-	i = 0;
-	while (l)
+	t = *l;
+	while (t)
 	{
-		i += ft_strlen((char*)l->content);
-		l = l->next;
+		if (t->content_size == (size_t)fd)
+		{
+			/*printf("Try free\n");
+			ft_memdel(&t->content);
+			printf("Free succ\n");*/
+			return (t);
+		
+		}
+		t = t->next;
 	}
-	ans = ft_strnew(i);
-	l = t;
-	i = -1;
-	while (l)
+	if ((t = ft_lstnew(NULL, 0)))
 	{
-		j = -1;
-		str = (char*)l->content;
-		if (str)
-			while (str[++j])
-				ans[++i] = str[j];
-		l = l->next;
+		t->content_size = (size_t)fd;
+		ft_lstpushback(l, t);
+		return (t);
 	}
-	return (ans);
+	return (NULL);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	static char	*ostatok = NULL;
-	char		*temp;
-	char		*cont;
-	char		buf[BUFF_SIZE + 1];
-	int			ret;
-	int			new_str_size;
-	t_list		*readlist;
+	static t_list	*fd_list = NULL;
+	t_list			*fd_elem;
+	char			*ostatok;
+	//static char		*ostatok = NULL;
+	char			*temp;
+	char			*cont;
+	char			buf[BUFF_SIZE + 1];
+	int				ret;
+	int				new_str_size;
+	t_list			*readlist;
 
-	new_str_size = 0;
 	if (BUFF_SIZE <= 0 || fd < 0 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
-	//printf("Ostatok 2 \"%s\"\n", ostatok);
+	new_str_size = 0;
+	fd_elem = get_list_elem(&fd_list, fd);
+	/*printf("Try free\n");
+	ft_memdel(&fd_elem->content);
+	printf("Free succ\n");*/
+	ostatok = ft_strdup((char*)fd_elem->content);
+	//printf("Ostatok at the beginning = \"%s\"\n             In list it is \"%s\"\n", ostatok, (char*)fd_elem->content);
 	if (ostatok && (cont = ft_memchr(ostatok, '\n', ft_strlen(ostatok))))
 	{
 		temp = ft_strdupab(ostatok, ft_strlen(ostatok) - ft_strlen(cont) + 1, ft_strlen(ostatok) - 1);
 		*line = ft_strdupab(ostatok, 0, ft_strlen(ostatok) - ft_strlen(cont) - 1);
 		free(ostatok);
 		//printf("Second half : \"%s\"\n", temp);
-		ostatok = temp[0] == '\0' ? NULL : ft_strdup(temp);
+		ft_memdel(&fd_elem->content);
+		fd_elem->content = temp[0] == '\0' ? NULL : (void*)ft_strdup(temp);
 		free(temp);
 		//printf("Res ostatok : \"%s\"\n", ostatok);
 		//printf("Returning 1 after nonempty ostatok\n");
+		//printf("Ostatok in the end = \"%s\"\n       In list it is \"%s\"\n", ostatok, (char*)fd_elem->content);
 		return (1);
 	}
 	readlist = ft_lstnew(ostatok, ft_strlen(ostatok) + 1);
@@ -85,19 +91,16 @@ int			get_next_line(const int fd, char **line)
 		if (ret != new_str_size)
 			break ;
 	}
-	*line = list_to_string(readlist);
+	*line = ft_list_to_string(readlist);
 	ft_lstdel(&readlist, NULL);
 	if (ostatok)
 		free(ostatok);
-	//printf("Ret = %d NSS = %d\n", ret, new_str_size);
-	//printf("Str = %s\n", *line);
 	if (ret == 0 && (*line)[0] == '\0')
-	{
-		//printf("Ostatok 1 \"%s\"\n", ostatok);
 		return (0);
-	}
-	ostatok = (new_str_size == ret || ret == 0)? NULL : ft_strdup(&buf[new_str_size + 1]);
-	//printf("Returning 1 after successfull ostatok\n");
-	//printf("Ostatok 1 \"%s\"\n", ostatok);
+	//printf("Ostatok in list before free \"%s\"\n", (char*)fd_elem->content);
+	ft_memdel(&fd_elem->content);
+	//printf("Free succ\n");
+	fd_elem->content = (new_str_size == ret || ret == 0)? NULL : (void*)ft_strdup(&buf[new_str_size + 1]);
+	//printf("Ostatok in the end \"%s\"\n", (char*)fd_elem->content);
 	return (1);
 }
